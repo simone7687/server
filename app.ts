8594f1fdb7eba8655e792c8e9d78c2730424dfa9
Simone Massaro <49060178+simone7687@users.noreply.github.com>
2023-05-25 21:25:17 +0200

    add type

--- app.ts
+++ app.ts
@@ -1,7 +1,16 @@
-const express = require('express');
-const app = express();
+import express, { Express, Request, Response } from 'express';
+import { House } from "./models/House";
+
+const app: Express = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+var houses: House[] = []
+
+// Creation of a house or a person
+app.post('/houses', (req: Request, res: Response) => {
+    let house = new House(1, "House 1", "City 1", "Address 1", new Date())
+    houses.push(house)
+    res.send(house)
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
